83f8c2e65c2f9c7d28c3faa647fd4ce91eef35b3
vadimshift <vadimshift@yandex.ru>
2021-04-14 17:06:50 +0500

    раскидал Апп по компонентам

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import Main from "./Main";
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__container">
+        <Header />
+        <Main />
+        <Footer />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
